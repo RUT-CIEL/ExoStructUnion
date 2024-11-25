238587702fe8cc720ba71b2a9b22a46eed954874
Timothé Rutily <timothe.rutily@gmail.com>
2024-11-25 16:26:00 +0100

    Ommit #3 : Programme de test contenu

--- Transgerbeur/Transgerbeurcpp.cpp
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -7,7 +7,7 @@ enum Eetat { vide, palette, roulea };
 struct Spalette
 {
 	int poids;
-	char* reference[19];
+	char reference[19];
 };
 struct Srouleau
 {
@@ -32,6 +32,26 @@ int main()
 {
 	cout << "-Transgerbeur : " << endl;
 
+	Scasier tabCasier[9];
 
-	
+	Srouleau ro1;
+	ro1.numero = 1;
+	ro1.longueur = 15.5;
+	Spalette pa1;
+	pa1.poids = 25;
+	strcpy_s(pa1.reference, "ref1");
+	Eetat etatC1;
+	etatC1 = vide;
+	Ucontenu cont;
+	cont.paletteCasier = pa1;
+	cont.rouleauCasier = ro1;
+	Scasier c1;
+	c1.etat = etatC1;
+	c1.px = 2;
+	c1.py = 3;
+	Scasier c2;
+	c2.etat = palette;
+	tabCasier[0] = c1;
+	tabCasier[2] = c2;
+
 }
