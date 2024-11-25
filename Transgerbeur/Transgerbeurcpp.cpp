d5a66719adb0cbf22b76f0f690233ff3d478ba30
Timothé Rutily <timothe.rutily@gmail.com>
2024-11-25 15:56:54 +0100

    commit #2 : déclaration des variables, enumerations et structures.

--- Transgerbeur/Transgerbeurcpp.cpp
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -2,8 +2,36 @@
 #include <iomanip>
 using namespace std;
 
+
+enum Eetat { vide, palette, roulea };
+struct Spalette
+{
+	int poids;
+	char* reference[19];
+};
+struct Srouleau
+{
+	float longueur;
+	int numero;
+};
+union Ucontenu
+{
+	Spalette paletteCasier;
+	Srouleau rouleauCasier;
+};
+struct Scasier
+{
+	int px, py;
+	Eetat etat;
+	Ucontenu cont;
+};
+
+
+
 int main()
 {
 	cout << "-Transgerbeur : " << endl;
 
+
+	
 }
